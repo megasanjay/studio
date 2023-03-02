e18bb3e789b451582c749e1f0408d30b1240eda5
Sanjay Soundarajan <sanjay.16@live.com>
2023-03-02 01:24:33 -0800

    dx: 🧑‍💻️ add config

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-undef
+const { commitlintConfig } = require("@fairdataihub/config");
+
+const config = commitlintConfig();
+
+// eslint-disable-next-line no-undef
+module.exports = config;
