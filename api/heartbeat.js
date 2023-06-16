9efa4f170acb28ca3071bca7384c7143111f2f84
Sanjay Soundarajan <sanjay.16@live.com>
2023-06-16 16:09:43 -0700

    fix: 🐛 update request method

--- api/heartbeat.js
+++ api/heartbeat.js
@@ -21,24 +21,24 @@ const dbName = process.env.MONGODB_DB;
 // Create a new MongoClient
 const client = new MongoClient(uri);
 
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
+// const allowCors = (fn) => async (req, res) => {
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   // another common pattern
+//   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+//   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+//   );
+//   if (req.method === "OPTIONS") {
+//     res.status(200).end();
+//     return;
+//   }
+//   return await fn(req, res);
+// };
 
-const handler = async (request, response) => {
+const heartbeat = async (request, response) => {
   console.log(request.method);
 
   if (request.method === "POST") {
@@ -63,7 +63,6 @@ const handler = async (request, response) => {
         });
 
         // delete the oldest heartbeat
-
         const heartbeatCount = await collection.countDocuments();
 
         if (heartbeatCount > 20) {
@@ -85,6 +84,6 @@ const handler = async (request, response) => {
   }
 };
 
-const heartbeat = allowCors(handler);
+// const heartbeat = allowCors(handler);
 
 export default heartbeat;
